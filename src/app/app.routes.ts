6b5d62a8ad74e8b98a96090119ed4eb224a6e7a4
Luiz Cruz <luizantoniojcruz@gmail.com>
2026-09-14 21:08:24 -0300

    feat: inclusão da nav-bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Cadastro } from './cadastro/cadastro';
+import { Cesta } from './cesta/cesta';
+import { Busca } from './busca/busca';
+import { Vitrine } from './vitrine/vitrine';
+import { Detalhe } from './detalhe/detalhe';
+import { Login } from './login/login';
+import { RecSenha } from './rec-senha/rec-senha';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"cadastro", component:Cadastro},
+    {path:"cesta", component:Cesta},
+    {path:"busca", component:Busca},
+    {path:"vitrine", component:Vitrine},
+    {path:"detalhes", component:Detalhe},
+    {path:"login", component:Login},
+    {path:"rec-senha", component:RecSenha}
+];
